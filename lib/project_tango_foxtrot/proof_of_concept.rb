ae4d2193ddbbbf171e9fdc3f8814766204a08da2
Josh Adams <josh@isotope11.com>
2012-04-30 00:53:41 -0500

    Fix stupid proof of concept bug, and switch to puma/sinatra (unnecessarily)

--- lib/project_tango_foxtrot/proof_of_concept.rb
+++ lib/project_tango_foxtrot/proof_of_concept.rb
@@ -1,18 +1,16 @@
-require 'reel'
+require 'sinatra'
 require File.expand_path("../echo.rb", __FILE__)
 
 include ProjectTangoFoxtrot
 
 port_number = 3001
 
-Reel::Server.supervise("0.0.0.0", port_number) do |connection|
-  request = connection.request
-
+get '/' do
   puts "loading contents async"
 
   contents = [
-    ['http://www.google.com', 0],
-    ['http://www.isotope11.com', 0.5]
+    ['immediate', 0],
+    ['delayed', 0.1]
   ]
   echos = contents.map{|u| Echo.new(u[0], u[1]) }
 
@@ -20,18 +18,13 @@ Reel::Server.supervise("0.0.0.0", port_number) do |connection|
   puts 'iterating'
   echos.each do |echo|
     echo.load_contents!
+  end
+  sleep 0.2
+
+  echos.each do |echo|
     content += "<h2>#{echo.future_contents}</h2>"
     content += echo.contents
   end
 
-  sleep 0.7
-
-  connection.respond :ok, content
-  puts 'responded'
-end
-
-puts "running on #{port_number}"
-
-while true do
-  # HACK
+  content
 end
